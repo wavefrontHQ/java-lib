922539a7b291ac0db5266af156001e2d2dc812a6
basilisk487 <vasily@wavefront.com>
2019-12-03 15:06:01 -0800

    Update to use batch events API

--- java-lib/src/main/java/com/wavefront/api/EventAPI.java
+++ java-lib/src/main/java/com/wavefront/api/EventAPI.java
@@ -1,19 +1,25 @@
 package com.wavefront.api;
 
 import com.wavefront.dto.EventDTO;
-import wavefront.report.Event;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/")
 public interface EventAPI {
 
+  /**
+   * Ingests and persists a batch of events.
+   *
+   * @param eventBatch batch of events to be reported
+   * @return HTTP response
+   */
   @Consumes(MediaType.APPLICATION_JSON)
   @POST
-  @Path("v2/event")
-  Response createEvent(EventDTO event);
+  @Path("v2/wfproxy/event")
+  Response reportEvents(List<EventDTO> eventBatch);
 }
