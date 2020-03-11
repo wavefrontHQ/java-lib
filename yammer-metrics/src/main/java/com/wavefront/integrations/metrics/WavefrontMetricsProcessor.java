ad855a7017a6d1d821b59317cc813d6b2ea3993c
Mike McMahon <mike.mcmahon@wavefront.com>
2020-03-11 11:30:55 -0700

    optimized imports for yammer-metrics

--- yammer-metrics/src/main/java/com/wavefront/integrations/metrics/WavefrontMetricsProcessor.java
+++ yammer-metrics/src/main/java/com/wavefront/integrations/metrics/WavefrontMetricsProcessor.java
@@ -3,7 +3,17 @@ package com.wavefront.integrations.metrics;
 import com.tdunning.math.stats.Centroid;
 import com.wavefront.common.MetricsToTimeseries;
 import com.wavefront.common.TaggedMetricName;
-import com.yammer.metrics.core.*;
+import com.yammer.metrics.core.Counter;
+import com.yammer.metrics.core.DeltaCounter;
+import com.yammer.metrics.core.Gauge;
+import com.yammer.metrics.core.Histogram;
+import com.yammer.metrics.core.Metered;
+import com.yammer.metrics.core.MetricName;
+import com.yammer.metrics.core.MetricProcessor;
+import com.yammer.metrics.core.Sampling;
+import com.yammer.metrics.core.Summarizable;
+import com.yammer.metrics.core.Timer;
+import com.yammer.metrics.core.WavefrontHistogram;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
