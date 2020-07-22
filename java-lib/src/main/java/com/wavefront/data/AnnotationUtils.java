1f339ec1316a1e722d0b2eaf6a7a5d2bac377703
basilisk487 <vasily@wavefront.com>
2020-07-22 08:52:07 -0500

    Updated as per code review

--- java-lib/src/main/java/com/wavefront/data/AnnotationUtils.java
+++ java-lib/src/main/java/com/wavefront/data/AnnotationUtils.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSetMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.SetMultimap;
@@ -21,9 +22,8 @@ public abstract class AnnotationUtils {
   private AnnotationUtils() {
   }
 
-  @Nullable
   public static SetMultimap<String, String> toMultimap(@Nullable Collection<Annotation> annotations) {
-    if (annotations == null) return null;
+    if (annotations == null) return ImmutableSetMultimap.of();
     final ImmutableSetMultimap.Builder<String, String> builder = ImmutableSetMultimap.builder();
     for (Annotation annotation : annotations) {
       builder.put(annotation.getKey(), annotation.getValue());
@@ -31,16 +31,14 @@ public abstract class AnnotationUtils {
     return builder.build();
   }
 
-  @Nullable
-  public static List<Annotation> toAnnotationList(Multimap<String, String> tags) {
-    if (tags == null || tags.isEmpty()) return null;
+  public static List<Annotation> toAnnotationList(@Nullable Multimap<String, String> tags) {
+    if (tags == null || tags.isEmpty()) return ImmutableList.of();
     return tags.entries().stream().map(t -> new Annotation(t.getKey(), t.getValue())).
         collect(Collectors.toList());
   }
 
-  @Nullable
-  public static List<Annotation> toAnnotationList(Map<String, String> tags) {
-    if (tags == null || tags.isEmpty()) return null;
+  public static List<Annotation> toAnnotationList(@Nullable Map<String, String> tags) {
+    if (tags == null || tags.isEmpty()) return ImmutableList.of();
     return tags.entrySet().stream().map(t -> new Annotation(t.getKey(), t.getValue())).
         collect(Collectors.toList());
   }
