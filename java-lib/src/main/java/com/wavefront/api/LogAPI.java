f7a8d6a8c35ec599a34e7d5e4e2d727aee93367c
German Laullon <glaullon@vmware.com>
2022-07-12 21:52:13 +0200

    Logs

--- java-lib/src/main/java/com/wavefront/api/LogAPI.java
+++ java-lib/src/main/java/com/wavefront/api/LogAPI.java
@@ -2,10 +2,7 @@ package com.wavefront.api;
 
 import com.wavefront.dto.Log;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -23,4 +20,10 @@ public interface LogAPI {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("le-mans/v1/streams/ingestion-pipeline-stream")
     Response proxyLogs(@HeaderParam("agent") final String agentProxyId, final List<Log> logBatch);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("le-mans/v1/streams/ingestion-pipeline-stream")
+    Response proxyLogsStr(@HeaderParam("agent") final String agentProxyId, final String logBatch);
 }
