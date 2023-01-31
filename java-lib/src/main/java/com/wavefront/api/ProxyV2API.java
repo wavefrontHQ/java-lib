19951b2f39f33ae2fe9063e2efd5bfcc5ed6c3d3
Joanna Ko <joannak@vmware.com>
2023-01-31 13:38:29 -0800

    MONIT-30817: Api to get and search Proxy configuration
    fix version

--- java-lib/src/main/java/com/wavefront/api/ProxyV2API.java
+++ java-lib/src/main/java/com/wavefront/api/ProxyV2API.java
@@ -49,6 +49,12 @@ public interface ProxyV2API {
                                   JsonNode agentMetrics,
                                   @QueryParam("ephemeral") Boolean ephemeral);
 
+  @POST
+  @Path("v2/wfproxy/saveConfig")
+  @Consumes(MediaType.APPLICATION_JSON)
+  void proxySaveConfig(@HeaderParam("X-WF-PROXY-ID") final UUID proxyId,
+                                  JsonNode proxyConfig);
+
   /**
    * Report batched data (metrics, histograms, spans, etc) to Wavefront servers.
    *
